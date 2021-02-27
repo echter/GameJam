5a46db1cfa61d0735343fc0645554ed8ed6d737c
miaolea <miaov@stud.ntnu.no>
2021-02-27 18:34:37 +0100

    adding windmill scripts

---
+++ Assets/Prefabs/FanModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System; 
+
+public class FanModel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    Rigidbody[] playerRigidbodyList; 
+
+    private int triggerDistanceZ = 20;
+    private int triggerDistanceX = 4; 
+    
+    void Start()
+    {
+     playerRigidbodyList = player.GetComponentsInChildren<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     float distZ = gameObject.transform.position.z - player.transform.position.z;
+     float distX = gameObject.transform.position.x - player.transform.position.x; 
+     bool isZActive = distZ > 0 && distZ <= triggerDistanceZ; 
+     bool isXActive = Math.Abs(distX) <= triggerDistanceX;
+     
+     if (isXActive && isZActive) 
+     {
+        foreach(Rigidbody rigidbody in playerRigidbodyList) 
+        {
+            rigidbody.AddForce(0,0,-1000);
+        }
+     }
+
+    }
+}
