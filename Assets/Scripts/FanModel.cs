7ecc7abeb22ed8d6640869f7481b246594c9d95d
miaolea <miaov@stud.ntnu.no>
2021-02-27 12:55:47 +0100

    windmill model start

---
+++ Assets/Scripts/FanModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanModel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    Rigidbody[] playerRigidbodyList; 
+    
+    void Start()
+    {
+     playerRigidbodyList = player.GetComponentsInChildren<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+     Debug.Log(player.transform.position);
+
+     foreach(Rigidbody rigidbody in playerRigidbodyList) {
+         rigidbody.AddForce(0,2000,0);
+     }
+
+    }
+}
